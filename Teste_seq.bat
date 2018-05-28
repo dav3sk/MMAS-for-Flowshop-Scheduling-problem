32ba30a55ac5993753bd43676ca63c77ded293d6
dav3sk <davibastos.v@gmail.com>
2018-05-28 14:07:57 -0300

    Refatorado Teste para receber arquivo

--- Teste_seq.bat
+++ Teste_seq.bat
@@ -4,13 +4,14 @@
 @SET N_FORMIGAS=5
 @SET TAXA_EVAPORACAO=0.75
 @SET GERACOES=10000
+@SET ARQUIVO="ta001.txt"
 
 @SET MSG= ========================== EXECUCAO #
 @echo off
 @FOR /l %%i in (1, 1, 30) DO (
 	echo .
 	echo %MSG%%%i ==========================
-	%ACO% %N_FORMIGAS% %TAXA_EVAPORACAO% %GERACOES%
+	%ACO% %N_FORMIGAS% %TAXA_EVAPORACAO% %GERACOES% %ARQUIVO%
 	echo .
 )
 
