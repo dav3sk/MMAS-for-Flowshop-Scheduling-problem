c1f74e730e51ecb6b6a019fd57058582728b002a
dav3sk <davibastos.v@gmail.com>
2018-07-10 13:37:49 -0300

    Adequado scripts de compilação para busca local

--- COMPILAEXECUTA.bat
+++ COMPILAEXECUTA.bat
@@ -1,5 +1,5 @@
 cls
-gcc Source/AntMain.c Source/Ant.c -std=c99 -o ACO -O3 -msse4
+gcc Source/AntMain.c Source/Ant.c Source/LocalSearch.c -std=c99 -o ACO -O3 -msse4
 pause
-ACO 5 0.75 10000 0.1 ta001.txt
+ACO 1 0.75 10000 0.1 ta001.txt
 pause
